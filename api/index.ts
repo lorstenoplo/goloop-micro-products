dffb9b4181ba685fbb8a3f80d8783f4530cb8429
nishanth <gayatridipali@gmail.com>
2021-04-06 19:16:32 +0530

    fixed cors

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,7 @@ import connectToDatabase from "../utils/connectToDb";
 export default async function (req: VercelRequest, res: VercelResponse) {
   const db = await connectToDatabase(process.env.MONGO_CONNECTION_URL);
   const collection = await db.collection("products");
-
   const products = await collection.find({}).toArray();
+
   res.status(200).json({ products });
 }
