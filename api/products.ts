42fe2c94aaf516751ceffd1a4df56e2da3b84f6a
nishanth <gayatridipali@gmail.com>
2021-04-06 15:21:21 +0530

    got products working

---
+++ api/products.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import connectToDatabase from "../utils/connectToDb";
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+  const db = await connectToDatabase(process.env.MONGO_CONNECTION_URL);
+  const collection = await db.collection("products");
+
+  const products = await collection.find({}).toArray();
+  res.status(200).json({ products });
+}
---
+++ utils/connectToDb.ts
@@ -0,0 +1,31 @@
+import url from "url";
+import MongoDb from "mongodb";
+
+const MongoClient = MongoDb.MongoClient;
+
+// Create cached connection variable
+let cachedDb = null;
+
+// A function for connecting to MongoDB,
+// taking a single parameter of the connection string
+export default async function connectToDatabase(uri: string) {
+  // If the database connection is cached,
+  // use it instead of creating a new connection
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  // If no connection is cached, create a new one
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // Select the database through the connection,
+  // using the database path of the connection string
+  const db = await client.db(url.parse(uri).pathname.substr(1));
+
+  // Cache the database connection and return the connection
+  cachedDb = db;
+  return db;
+}
