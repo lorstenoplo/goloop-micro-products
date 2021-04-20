c3e52cf2dcc0f2e6110f2454f24dd2ccda4667ff
nishanth <gayatridipali@gmail.com>
2021-04-20 10:40:34 +0530

    changed to slug

--- api/productsBySlug/[slug].ts
+++ api/productsBySlug/[slug].ts
@@ -3,10 +3,10 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import connectToDatabase from "../../utils/connectToDb";
 
 export default async function (req: VercelRequest, res: VercelResponse) {
-  const { category } = req.query;
+  const { slug } = req.query;
   const db = await connectToDatabase(process.env.MONGO_CONNECTION_URL);
   const collection = db.collection("products");
-  const products = await collection.find({ category }).toArray();
+  const products = await collection.find({ slug }).toArray();
 
   res.status(200).json({ products });
 }
